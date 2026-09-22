853fe5167f76e79873a113aef89123b755fc1daa
tion-jiale <tionjiale@gmail.com>
2026-09-22 10:20:30 +0800

    Update Power BI embed URL in Tourist Analysis

--- pages/3_Tourist Analysis.py
+++ pages/3_Tourist Analysis.py
@@ -13,7 +13,7 @@ st.title("Tourist Analysis Dashboard")
 st.caption("created by CODE RED")
 
 POWER_BI_EMBED_URL = (
-    "https://app.powerbi.com/links/jWvHi_JUcY?ctid=7f048fc1-2ea3-48e4-ac92-91d1eb09807c&pbi_source=linkShare&bookmarkGuid=ac8c7c64-a6cc-4a1e-8b92-0d013c7873ab"
+    "https://app.powerbi.com/reportEmbed?reportId=91d9806a-b675-4c8f-aceb-d17c794ac5a5&autoAuth=true&ctid=7f048fc1-2ea3-48e4-ac92-91d1eb09807c"
 )
 
 components.iframe(
