8bd9d98a48b485211971ce56210531b85c85538b
tion-jiale <tionjiale@gmail.com>
2026-09-22 10:16:47 +0800

    Update 3_Tourist Analysis.py

--- pages/3_Tourist Analysis.py
+++ pages/3_Tourist Analysis.py
@@ -13,10 +13,7 @@ st.title("Tourist Analysis Dashboard")
 st.caption("created by CODE RED")
 
 POWER_BI_EMBED_URL = (
-    "https://app.powerbi.com/reportEmbed"
-    "?reportId=91d9806a-b675-4c8f-aceb-d17c794ac5a5"
-    "&autoAuth=true"
-    "&ctid=7f048fc1-2ea3-48e4-ac92-91d1eb09807c"
+    "https://app.powerbi.com/groups/me/reports/91d9806a-b675-4c8f-aceb-d17c794ac5a5?ctid=7f048fc1-2ea3-48e4-ac92-91d1eb09807c&pbi_source=linkShare"
 )
 
 components.iframe(
