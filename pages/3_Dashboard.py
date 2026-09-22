8301dd0067f4e049657abc773b32c6f7345cbb3a
tion-jiale <tionjiale@gmail.com>
2026-09-22 09:45:59 +0800

    Create 3_Dashboard.py

---
+++ pages/3_Dashboard.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+from shared import styled_box
+
+st.set_page_config(
+    page_title="Dashboard",
+    layout="wide",
+    page_icon="📊"
+)
+
+st.title("📊 Dashboard")
+st.caption("LATEST CODE RED DASHBOARD")
+
+POWER_BI_EMBED_URL = (
+    "https://app.powerbi.com/reportEmbed"
+    "?reportId=91d9806a-b675-4c8f-aceb-d17c794ac5a5"
+    "&autoAuth=true"
+    "&ctid=7f048fc1-2ea3-48e4-ac92-91d1eb09807c"
+)
+
+components.iframe(
+    POWER_BI_EMBED_URL,
+    height=800,
+    scrolling=False
+)
